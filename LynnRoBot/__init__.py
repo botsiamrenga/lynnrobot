167e3b29d69071e05801714d7b3e2ac2adac6972
didikteatochhawng <81432899+didikteatochhawng@users.noreply.github.com>
2021-06-07 16:33:27 +0530

    Update __init__.py

--- LynnRoBot/__init__.py
+++ LynnRoBot/__init__.py
@@ -193,8 +193,8 @@ else:
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("masha", API_ID, API_HASH)
-pbot = Client("mashapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+telethn = TelegramClient("lynn", API_ID, API_HASH)
+pbot = Client("lynnpbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
