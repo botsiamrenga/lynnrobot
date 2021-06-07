194ef8e0ab0319c2877f865828176b21b5b0d877
didikteatochhawng <81432899+didikteatochhawng@users.noreply.github.com>
2021-06-07 10:23:11 +0530

    Create __musicals.py

---
+++ LynnRoBot/modules/__musicals.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Songs"
+
+__help__ = """
+ ♬ /song <song name>*:* Uploads the song in it's best quality available
+ Eg: `/song Bohemian rhapsody`
+ ♬ /lyrics <song name>*:* provides the lyrics of the song you want.
+ Eg: `/lyrics Faded`
+ ♬ /video <song name>*:* uploads the video song in it's best quality available.
+ Eg:  `/video Bohemian rhapsody`
+"""
